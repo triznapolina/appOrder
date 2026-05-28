1d603d602a93182b507376d993d6f32e9d648a08
trpoliSo <tryznapalina@gmail.com>
2026-05-28 09:51:21 +0300

    last changes

--- src/main/java/com/config/OpenApiConfig.java
+++ src/main/java/com/config/OpenApiConfig.java
@@ -1,9 +1,5 @@
 package com.config;
 
-
-
-
-
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
--- src/main/java/com/config/WebConfig.java
+++ src/main/java/com/config/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173") // React app's URL
+                .allowedOrigins("http://localhost:5173")
                 .allowedMethods("GET", "POST","PATCH", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
